84f1aed1adcc8425c4ebebfd71c2d8dcfbe6dc5d
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-02-21 19:06:37 -0600

    Address and Employee classes implemented

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String designation;
+    private double salary;
+    Address address;
+
+    public Employee(String id, String name, String designation, double salary, Address address) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                ", salary= $" + salary +
+                ", address=" + address +
+                '}';
+    }
+}
